3520df742b71cad6a723d65caf9fbf1f298e7c85
poton8 <pagilestennis@gmail.com>
2019-06-25 21:03:35 -0500

    turned the mounted function into an async function

--- VueJsRenderJSON.js
+++ VueJsRenderJSON.js
@@ -7,20 +7,15 @@ Vue.component('render-json', {
     },
     template: '<p>{{message}}</p>',
 
-    mounted(){
-        const request = async () => {
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-            const json = await response.json();
-            this.message = json
-        }
-        request()
+    async mounted() {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const json = await response.json();
+        this.message = json
     }
     
     
   });
 
 new Vue({
-    el: '#render',
-    
+    el: '#render'
 });
-
